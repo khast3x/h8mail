07fe61261b45c0ea5934fe396eaf59c57a692f3d
khast3x <1370650+khast3x@users.noreply.github.com>
2019-05-22 01:55:30 +0200

    fix history remved & ifelif leaklookup

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ from setuptools import setup, find_packages
 with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open("HISTORY.rst") as history_file:
-    history = history_file.read()
+# with open("HISTORY.rst") as history_file:
+#     history = history_file.read()
 
 requirements = ['requests']
 
@@ -32,7 +32,8 @@ setup(
     description="Email OSINT and password breach hunting. Use h8mail to find passwords through different breach and reconnaissance services, or the infamous Breached Compilation torrent",
     install_requires=requirements,
     license="BSD license",
-    long_description=readme + "\n\n" + history,
+    long_description=readme + "\n\n",
+    # long_description=readme + "\n\n" + history,
     include_package_data=True,
     keywords="h8mail",
     name="h8mail",

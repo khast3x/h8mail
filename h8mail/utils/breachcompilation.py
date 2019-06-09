e37b5b5f9b92149bc76d09a07de0a08f5660f7c7
khast3x <1370650+khast3x@users.noreply.github.com>
2019-06-09 21:18:07 +0200

    fix encoding issues & missing bash

--- h8mail/utils/breachcompilation.py
+++ h8mail/utils/breachcompilation.py
@@ -1,29 +1,34 @@
 # -*- coding: utf-8 -*-
 from .colors import colors as c
 import os
-
+import re
+import stat
+from string import printable
 
 def breachcomp_check(targets, breachcomp_path):
     # https://gist.github.com/scottlinux/9a3b11257ac575e4f71de811322ce6b3
     try:
         import subprocess
-
-        c.info_news("Looking up targets in BreachCompilation")
         query_bin = os.path.join(breachcomp_path, "query.sh")
-        subprocess.call(["chmod", "+x", query_bin])
+        st = os.stat(query_bin)
+        os.chmod(query_bin, st.st_mode | stat.S_IEXEC)
         for t in targets:
+            c.info_news(f"Looking up {t.email} in BreachCompilation")  
             procfd = subprocess.run([query_bin, t.email], stdout=subprocess.PIPE)
+            print(procfd)
             try:
-                output = procfd.stdout.decode("utf-8")
+                output = procfd.stdout.decode("cp437")
             except Exception as e:
                 c.bad_news(f"Could not decode bytes for {t.email} results")
                 output = procfd.stdout
-                print(output[:85], "[...]")
+                # print(output[:85], "[...]")
+                print(output)
                 continue
             if len(output) != 0:
                 split_output = output.split("\n")
                 for line in split_output:
                     if ":" in line:
+                        t.pwned = True
                         t.data.append(("BC_PASS", line.split(":")[1]))
                         c.good_news(
                             "Found BreachedCompilation entry {line}".format(line=line)
@@ -32,3 +37,4 @@ def breachcomp_check(targets, breachcomp_path):
     except Exception as e:
         c.bad_news("Breach compilation")
         print(e)
+        return targets
